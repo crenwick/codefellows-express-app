a745765e60a440c7291abb54e60526c4ddb418e7
Charles Renwick <crenwick@Charless-MacBook-Pro-2.local>
2014-10-31 13:35:29 -0700

    this is the files to run the server.

---
+++ server.js
@@ -0,0 +1,12 @@
+/*jshint node: true*/
+'use strict';
+var express = require('express'),
+    app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('*', function (req, res) {
+    res.redirect('/contact.html');
+});
+
+app.listen(process.env.PORT || 3000);
