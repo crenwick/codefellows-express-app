41d7889f9ef60f186812db1395b784d7901c779d
Charles Renwick <crenwick@Charless-MacBook-Pro-2.local>
2014-11-01 22:47:55 -0700

    added jcsc to the grunt test

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,25 @@
 
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-jscs');
 
     grunt.initConfig({
 	pkg: grunt.file.readJSON('package.json'),
+	jscs: {
+	    all: {
+		options: {
+		    'standard': 'Idiomatic',
+		    'reportFull': true
+		},
+		files: {
+		    src: ['server.js']
+		}
+	    }
+	},
 	jshint: {
-	    all: ['lib/*.js']
+	    all: ['server.js']
 	}
     });
-    grunt.registerTask('test', ['jshint']);
+    grunt.registerTask('test', ['jshint', 'jscs']);
     grunt.registerTask('default', ['test']);
 };
